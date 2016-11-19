2839f411fd1dcb4111dcd1a1695c8b50eaf3de9f
Tarik Larbi <t.larbi@ext.bouygues-immobilier.com>
2016-11-19 22:33:02 +0100

    first commit

--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var helloLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        helloLabel.textColor = UIColor.red
     }
 
     override func didReceiveMemoryWarning() {
